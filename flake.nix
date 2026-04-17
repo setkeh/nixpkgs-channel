33a45805d20f427b72f95251e2e6d1ff29476462
James SETKEH Griffis <setkeh@gmail.com>
2026-04-18 01:59:30 +1000

    Im a silly sausage and copy/pastaed

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     systems.url = "github:nix-systems/default";
     scikit-learn-1-2.url = "github:NixOS/nixpkgs/e08e100b0c6a6651c3235d1520c53280142d9d5f";
   };
@@ -13,10 +13,10 @@
 
       imports = [
         inputs.flake-parts.flakeModules.easyOverlay
-        ./imports/overlay.nix
-        ./imports/formatter.nix
-        ./imports/pkgs-by-name.nix
-        ./imports/pkgs-all.nix
+        ./modules/overlay.nix
+        ./modules/formatter.nix
+        ./modules/pkgs-by-name.nix
+        ./modules/pkgs-all.nix
       ];
     };
 }
