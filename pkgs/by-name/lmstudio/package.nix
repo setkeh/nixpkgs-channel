e072c8f215ce985235867ddbe67f6e7862aa61f6
James SETKEH Griffis <setkeh@gmail.com>
2026-04-18 05:23:50 +1000

    adding latest Version of LMStudio

---
+++ pkgs/by-name/lmstudio/package.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  stdenv,
+  callPackage,
+  ...
+}@args:
+let
+  pname = "lmstudio";
+
+  version_x86_64-linux = "0.4.12-1";
+  hash_x86_64-linux = "sha256-0Y4XjK3vfWeY8Z5tQfM6KX4modKFCRy8MNqCUtGKRvA=";
+
+  meta = {
+    description = "LM Studio is an easy to use desktop app for experimenting with local and open-source Large Language Models (LLMs)";
+    homepage = "https://lmstudio.ai/";
+    license = lib.licenses.unfree;
+    mainProgram = "lm-studio";
+    maintainers = with lib.maintainers; [ crertel ];
+    platforms = [
+      "x86_64-linux"
+      "aarch64-darwin"
+    ];
+    sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
+    broken = stdenv.hostPlatform.isDarwin; # Upstream issue: https://github.com/lmstudio-ai/lmstudio-bug-tracker/issues/347
+  };
+in
+if stdenv.hostPlatform.isDarwin then
+  callPackage ./linux.nix {
+    inherit pname meta;
+    passthru.updateScript = ./update.sh;
+    version = version_x86_64-linux;
+    url =
+      args.url
+        or "https://installers.lmstudio.ai/linux/x64/${version_x86_64-linux}/LM-Studio-${version_x86_64-linux}-x64.AppImage";
+    hash = args.hash or hash_x86_64-linux;
+  }
