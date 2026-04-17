73bcb6f1603653c86bfb379ea0922e819b8868e1
James SETKEH Griffis <setkeh@gmail.com>
2026-04-18 02:09:50 +1000

    Im a silly sausage and copy/pastaed

--- modules/pkgs-all.nix
+++ modules/pkgs-all.nix
@@ -1,16 +1,9 @@
-{ inputs, ... }:
-
+{ ... }:
 {
+
   perSystem =
-    { config, system, ... }:
+    { ... }:
     {
-      _module.args.pkgs = import inputs.nixpkgs {
-        inherit system;
-        overlays = [
-          (_final: _prev: {
-            local = config.packages;
-          })
-        ];
-      };
+      packages = { };
     };
 }
